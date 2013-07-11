ae7f8417187268d53125fd7ead3ea88391314dd4
Prabu Venkatesan <pvenkatesan@stanford.edu>
2013-07-11 03:08:13 +0000

    Updated web.js to read from index.html

--- web.js
+++ web.js
@@ -2,8 +2,15 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+var infile="index.html";
+
+var buffer = fs.readFileSync(infile, null);
+
+ console.log("buffervalue " + buffer.toString());
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
